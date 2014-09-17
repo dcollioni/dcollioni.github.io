5a6a3a363ce6751009d871c03d3fd117fc50485d
Douglas Collioni <dcollioni@gmail.com>
2014-09-17 13:24:50 -0300

    Implementing AngularJS

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+﻿var dcollioniApp = angular.module('dcollioniApp', ['ngRoute']);
+
+// configure our routes
+dcollioniApp.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+
+        .when('/home', {
+            templateUrl: 'pages/home.html',
+            controller: 'mainController'
+        })
+
+        .when('/repos', {
+            templateUrl: 'pages/home.html',
+            controller: 'reposController'
+        })
+
+        .when('/about', {
+            templateUrl: 'pages/home.html',
+            controller: 'aboutController'
+        })
+
+        .when('/contact', {
+            templateUrl: 'pages/home.html',
+            controller: 'contactController'
+        });
+
+    $locationProvider.html5Mode(true);
+});
+
+dcollioniApp.controller('mainController', function ($scope) {
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+dcollioniApp.controller('reposController', function ($scope) {
+    $scope.message = 'My repos here...';
+});
+
+dcollioniApp.controller('aboutController', function ($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+dcollioniApp.controller('contactController', function ($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
