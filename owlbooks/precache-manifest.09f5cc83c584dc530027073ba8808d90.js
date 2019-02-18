84121cc926454866114fa0b8fab7d963e0235455
Douglas Collioni <douglas.collioni@sap.com>
2019-02-18 14:07:01 -0300

    add owlbooks folder

---
+++ owlbooks/precache-manifest.09f5cc83c584dc530027073ba8808d90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f934a9c71e9d0062813f",
+    "url": "/owlbooks/static/js/runtime~main.f934a9c7.js"
+  },
+  {
+    "revision": "1e9eb0f6be6ddd7429fd",
+    "url": "/owlbooks/static/js/main.1e9eb0f6.chunk.js"
+  },
+  {
+    "revision": "9543fee2f9bef1c30f01",
+    "url": "/owlbooks/static/js/1.9543fee2.chunk.js"
+  },
+  {
+    "revision": "1e9eb0f6be6ddd7429fd",
+    "url": "/owlbooks/static/css/main.5e454fc5.chunk.css"
+  },
+  {
+    "revision": "3eb954744eb168ddbc6ed5611525bc7b",
+    "url": "/owlbooks/index.html"
+  }
+];
